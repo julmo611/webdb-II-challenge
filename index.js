87c012b30e4b653fdf61af39347382d98d3a127b
Julian Moreno <julmo611@gmail.com>
2019-03-26 14:26:09 -0700

    Bears teble

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const zoosRouter = require('./zoos/zoos-router.js');
+const bearsRouter = require('./zoos/bears-router.js');
 
 const server = express();
 
@@ -8,6 +9,7 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 server.use('/api/zoos', zoosRouter);
+server.use('/api/bears', bearsRouter);
 
 const port = 3300;
 server.listen(port, function() {
